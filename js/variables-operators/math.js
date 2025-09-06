28af8eebcc38a417a2c7c4acef5eb4ced89b3068
Dorukan <catakd@mef.edu.tr>
2025-09-06 21:13:01 +0300

    add js variables-operators exercise

---
+++ js/variables-operators/math.js
@@ -0,0 +1,28 @@
+console.log(23+97);
+
+console.log(14 + 191 + 76 + 12 + 87 + 2);
+
+console.log((4 + 6 + 9) / 77);
+
+// let's use some variables
+
+let a = 10;
+
+console.log(a);
+
+a = 42
+
+console.log(a);
+
+let b = 7 * a;
+
+console.log(b);
+
+// constants
+
+const max = 57;
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);
