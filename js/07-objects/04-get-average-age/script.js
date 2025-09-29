87d858e0369b96d1c4b3ce5c6f32ebc423a658d8
Dorukan <catakd@mef.edu.tr>
2025-09-29 12:00:20 +0300

    add alternative sol w/ reduce method

--- js/07-objects/04-get-average-age/script.js
+++ js/07-objects/04-get-average-age/script.js
@@ -4,10 +4,15 @@ let mary = { name: "Mary", age: 29 };
 
 let arr = [ john, pete, mary ];
 
-function getAverageAge(arr){
-    let totalAge = 0
-    arr.map(item => totalAge += item.age);
-    return (totalAge / arr.length);
+// function getAverageAge(arr){
+//     let totalAge = 0
+//     arr.map(item => totalAge += item.age);
+//     return (totalAge / arr.length);
+// }
+
+// alternative sol
+function getAverageAge(users){
+    return users.reduce((prev, user) => prev + user.age, 0) / users.length;
 }
 
 console.log(getAverageAge(arr));
